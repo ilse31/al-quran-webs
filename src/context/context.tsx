b0de406c2fc11ad5c81409f45501d81db7ba7472
ilse31 <ilhamprazsetyo33@gmail.com>
2023-03-28 14:35:29 +0700

    add

---
+++ src/context/context.tsx
@@ -0,0 +1,86 @@
+import React, { createContext, useContext, useReducer, useEffect } from "react";
+
+// Membuat tipe untuk state favorites
+export type Favorite = {
+  nomor: number;
+  nama: string;
+  nama_latin: string;
+  jumlah_ayat: number;
+  tempat_turun: string;
+  arti: string;
+  deskripsi: string;
+};
+
+export type FavoritesState = {
+  favorites: Favorite[];
+};
+
+// Inisialisasi state awal
+export const initialState: FavoritesState = {
+  favorites: [],
+};
+
+// Membuat tipe untuk action
+type Action =
+  | { type: "ADD_FAVORITE"; payload: Favorite }
+  | { type: "REMOVE_FAVORITE"; payload: number };
+
+// Reducer untuk mengubah state favorites
+function favoritesReducer(state: FavoritesState, action: Action) {
+  switch (action.type) {
+    case "ADD_FAVORITE":
+      // Jika buku belum ada di favorites, tambahkan ke favorites
+      if (
+        !state.favorites.find(
+          (favorite) => favorite.nomor === action.payload.nomor
+        )
+      ) {
+        return { ...state, favorites: [...state.favorites, action.payload] };
+      }
+      return state;
+    case "REMOVE_FAVORITE":
+      // Filter buku yang akan dihapus dari favorites
+      const newFavorites = state.favorites.filter(
+        (favorite) => favorite.nomor !== action.payload
+      );
+      return { ...state, favorites: newFavorites };
+    default:
+      return state;
+  }
+}
+
+// Membuat context favorites
+export const FavoritesContext = createContext<{
+  state: FavoritesState;
+  dispatch: React.Dispatch<Action>;
+}>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+// Membuat custom hook untuk mengakses context favorites
+export const useFavorites = () => useContext(FavoritesContext);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+// Component provider untuk favorites context
+export const FavoritesProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(favoritesReducer, initialState, () => {
+    // Ambil data favorites dari local storage saat pertama kali rendering
+    const favorites = localStorage.getItem("favorites");
+    return favorites ? { favorites: JSON.parse(favorites) } : initialState;
+  });
+
+  // Simpan data favorites ke local storage setiap kali terjadi perubahan pada state
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(state.favorites));
+  }, [state]);
+
+  return (
+    <FavoritesContext.Provider value={{ state, dispatch }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
