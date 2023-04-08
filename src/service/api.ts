a4fc758f04e1690846bacfeb7bedbcf6e0c4afdf
ilse31 <ilhamprazsetyo33@gmail.com>
2023-04-08 21:30:31 +0700

    add path

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "https://al-quran-webs.vercel.app/api/",
   headers: {
     "Content-Type": "application/json",
   },
