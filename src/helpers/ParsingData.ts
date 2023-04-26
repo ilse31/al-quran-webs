6778d75938a379051a2f786dd2baaeabb7c652c9
ilse31 <ilhamprazsetyo33@gmail.com>
2023-04-26 10:33:47 +0700

    fiks converter

--- src/helpers/ParsingData.ts
+++ src/helpers/ParsingData.ts
@@ -8,21 +8,6 @@ const stringToHTML = (str: string) => {
   return parse(str);
 };
 
-function convertDigitEntoArabic(input: string): string {
-  const arabicDigits = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
-  let output = "";
-  for (let i = 0; i < input.length; i++) {
-    const char = input[i];
-    const charIndex = arabicDigits.indexOf(char);
-    if (charIndex !== -1) {
-      output += charIndex;
-    } else {
-      output += char;
-    }
-  }
-  return output;
-}
-
 const ConvertToArabicNumbers = (num: number) => {
   const arabicNumbers =
     "\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669";
@@ -31,9 +16,4 @@ const ConvertToArabicNumbers = (num: number) => {
   });
 };
 
-export {
-  htmlParser,
-  stringToHTML,
-  convertDigitEntoArabic,
-  ConvertToArabicNumbers,
-};
+export { htmlParser, stringToHTML, ConvertToArabicNumbers };
